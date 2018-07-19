b8e92463694aeb0577e55cba8856fde54e7097eb
Ray Blick <rblick.ecol@gmail.com>
2018-07-19 19:27:18 +1000

    updated json format

--- hops.js
+++ hops.js
@@ -1,174 +1,23 @@
-[
-    {  
-      name: "Australian",
-      id: 1,
-      children: [{
-          name: "Pride of Ringwood",
-          id: 101,
-          alpha: [8.6, 10.5],
-          beta: [4.4, 7],
-          oil: [1.6, 1.8],
-          commercial: ['Ale', 'IPA'],
-          aroma: ['Herbal', 'Cedar', 'Oak'],
-          hopType: 'Aroma'
-        },{
-          name: "Melba",
-          id: 102,
-        },{
-          name: "Vic Secret",
-          id: 103,
-        },{
-          name: "Topaz",
-          id: 104,
-        },{
-          name: "Sylva",
-          id: 105,
-        },{
-          name: "Summer",
-          id: 106,
-        },{
-          name: "Helga",
-          id: 107,
-        },{
-          name: "Galaxy",
-          id: 108,
-        },{
-          name: "Ella",
-          id: 109,
-        },{
-          name: "Enigma",
-          id: 110,
-        }]
-    },
-    {
-      name: "American",
-      id: 2,
-      children: [{
-          name: "Cascade",
-          id: 201,
-        },{
-          name: "Willamette",
-          id: 202,
-        },{
-          name: "Warrior",
-          id: 203,
-        },{
-          name: "Simcoe",
-          id: 204,
-        },{
-          name: "Mosaic",
-          id: 205,
-        },{
-          name: "El Dorado",
-          id: 206,
-        },{
-          name: "Columbus",
-          id: 207,
-        },{
-          name: "Cluster",
-          id: 208,
-        },{
-          name: "Citra",
-          id: 209,
-        },{
-          name: "Chinook",
-          id: 210,
-        },{
-          name: "Centennial",
-          id: 211,
-        },{
-          name: "Amarillo",
-          id: 212,
-        },{
-          name: "Idaho",
-          id: 213,
-        }]
-    },
-    {
-      name: "European",
-      id: 3,
-      children: [{
-          name: "Hallertau",
-          id: 301,
-        },{
-          name: "Mandarina",
-          id: 302,
-        },{
-          name: "Tettnang",
-          id: 303,
-        },{
-          name: "Styrian Goldings",
-          id: 304,
-        },{
-          name: "Saaz",
-          id: 305,
-        },{
-          name: "Perle",
-          id: 306,
-        },{
-          name: "Northern Brewer",
-          id: 307,
-        },{
-          name: "Magnum",
-          id: 308,
-        },{
-          name: "Hersbrucker",
-          id: 309,
-        },{
-          name: "Hallertau Blanc",
-          id: 310,
-        }]
-    },
-    {
-      name: "British",
-      id: 4,
-      children: [{
-          name: "Challenger",
-          id: 401,
-        },{
-          name: "East Kent Goldings",
-          id: 402,
-        },{
-          name: "Fuggles",
-          id: 403,
-        },{
-          name: "Northdown",
-          id: 404,
-        }]
-    },
-    {
-      name: "New Zealand",
-      id: 5,
-      children: [{
-          name: "Dr Rudi",
-          id: 501,
-        },{
-          name: "Taiheke",
-          id: 502,
-        },{
-          name: "Moutere",
-          id: 503,
-        },{
-          name: "Riwaka",
-          id: 504,
-        },{
-          name: "Wakatu",
-          id: 505,
-        },{
-          name: "Wai-iti",
-          id: 506,
-        },{
-          name: "Nelson Sauvin",
-          id: 507,
-        },{
-          name: "Motueka",
-          id: 508,
-        },{
-          name: "Green Bullet",
-          id: 509,
-        },{
-          name: "Waimea",
-          id: 510,
-        }]
-    },
-  ]
+{  
+  "name": "Australian",
+  "id": 1,
+  "children": [{
+      "name": "Pride of Ringwood",
+      "id": 101,
+      "alpha": [8.6, 10.5],
+      "beta": [4.4, 7],
+      "oil": [1.6, 1.8],
+      "commercial": ['Ale', 'IPA'],
+      "aroma": ['Herbal', 'Cedar', 'Oak'],
+      "hopType": 'Aroma'
+    },{
+      "name": "Melba",
+      "id": 102,
+    },{
+      "name": "Vic Secret",
+      "id": 103,
+    },{
+      "name": "Topaz",
+      "id": 104,
+    }]
+}
